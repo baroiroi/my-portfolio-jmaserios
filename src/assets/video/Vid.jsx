0b51c819b80c8a5ba06ea1a5326d4677f4f8dc1d
baroiroi <johnroy0414@gmail.com>
2023-08-12 22:35:55 +0800

    updated Vid.jsx video should be muted

--- src/assets/video/Vid.jsx
+++ src/assets/video/Vid.jsx
@@ -4,7 +4,7 @@ import Johnroy from "./Johnroy.mp4"
 export const Vid = () => {
   return (
     <>
-      <video autoPlay loop>
+      <video autoPlay loop muted className='flex justify-center items-center'>
         <source src={Johnroy} type="video/mp4"/>
       </video>
     </>
