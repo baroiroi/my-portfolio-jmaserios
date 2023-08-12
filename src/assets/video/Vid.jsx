3e6bc76ca041b4ff38ea16cb4ea7fc4a138e4c0c
baroiroi <johnroy0414@gmail.com>
2023-08-12 22:09:19 +0800

    edited Vid.jsx

--- src/assets/video/Vid.jsx
+++ src/assets/video/Vid.jsx
@@ -3,10 +3,10 @@ import Johnroy from "./Johnroy.mp4"
 
 export const Vid = () => {
   return (
-    <div>
+    <>
       <video autoPlay loop>
         <source src={Johnroy} type="video/mp4"/>
       </video>
-    </div>
+    </>
   )
 }
