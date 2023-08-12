305fb458000d3dc641ea07c241950ea6905dfa07
baroiroi <johnroy0414@gmail.com>
2023-08-12 14:02:22 +0800

    add data.jsx

---
+++ src/utils/data.jsx
@@ -0,0 +1,26 @@
+export const navbar = [
+    {
+      name: 'Splash',
+      href: '/splash'
+    },
+    {
+      name: 'About',
+      href: '/about'
+    },
+    {
+      name: 'Skills',
+      href: '/skills'
+    },
+    {
+      name: 'Experiences',
+      href: '/experiences'
+    },
+    {
+      name: 'Projects',
+      href: '/projects'
+    },
+    {
+      name: 'Contacts',
+      href: '/contacts'
+    },
+]
