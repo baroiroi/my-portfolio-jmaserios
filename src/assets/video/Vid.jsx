8384ddb892492b7da6f02f4a1a9b4cb678bf51e3
baroiroi <johnroy0414@gmail.com>
2023-08-13 08:20:09 +0800

    update on logo

--- src/assets/video/Vid.jsx
+++ src/assets/video/Vid.jsx
@@ -4,8 +4,8 @@ import Johnroy from "./Johnroy.mp4"
 export const Vid = () => {
   return (
     <>
-      <video autoPlay loop muted className='flex justify-center items-center'>
-        <source src={Johnroy} type="video/mp4"/>
+      <video autoPlay loop muted className='flex items-center justify-center h-[3rem]'>
+        <source src={Johnroy} type="video/mp4" />
       </video>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,38 @@
 import React, { useState } from 'react'
 import { SiBurgerking } from 'react-icons/si'
 import { navbar } from '../utils/data'
+import { Vid } from '../assets/video/Vid'
 const NavBar = () => {
 
-    const [showMenu,setShowMenu] = useState(false)
+    const [showMenu, setShowMenu] = useState(false)
 
     const handleClick = () => {
-        setShowMenu(prev => !prev)   
+        setShowMenu(prev => !prev)
     }
 
 
-  return (
-    <div className="">
-    <div className="flex items-center justify-between h-10 bg-white px-3">
-        <span>Logo</span>
-        <SiBurgerking size={30} onClick={handleClick} />
-    </div>
-    {showMenu && <div className="fixed px-3 text-white text-right h-full w-full opacity-0.5">
-        <ul className="">
-        {navbar.map((nav,index) => {
-            return(
-                <li key={index}>
-                    <a href={nav.href}>{nav.name}</a>
-                </li>
-            )
-        })
-        }
-        </ul>
-    </div>}
-    </div>
-  )
+    return (
+        <div className="">
+            <div className="flex items-center justify-between px-3 bg-white h-15">
+                <span>
+                    <Vid />
+                </span>
+                <SiBurgerking size={30} onClick={handleClick} />
+            </div>
+            {showMenu && <div className="fixed px-3 text-white text-right h-full w-full opacity-0.5">
+                <ul className="">
+                    {navbar.map((nav, index) => {
+                        return (
+                            <li key={index}>
+                                <a href={nav.href}>{nav.name}</a>
+                            </li>
+                        )
+                    })
+                    }
+                </ul>
+            </div>}
+        </div>
+    )
 }
 
 export default NavBar
