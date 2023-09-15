67e728a36982bfda24229f304d7e76a3228fb0ad
baroiroi <johnroy0414@gmail.com>
2023-09-15 20:59:47 +0800

    updated my navbar

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -1,26 +1,26 @@
 export const navbar = [
-    {
-      name: 'Home',
-      href: '/splash'
-    },
-    {
-      name: 'About',
-      href: '/about'
-    },
-    {
-      name: 'Skills',
-      href: '/skills'
-    },
-    {
-      name: 'Experiences',
-      href: '/experiences'
-    },
-    {
-      name: 'Projects',
-      href: '/projects'
-    },
-    {
-      name: 'Contacts',
-      href: '/contacts'
-    },
+  {
+    name: 'Home',
+    href: '/splash'
+  },
+  {
+    name: 'About',
+    href: '/about'
+  },
+  {
+    name: 'Skills',
+    href: '/skills'
+  },
+  {
+    name: 'Experiences',
+    href: '/experiences'
+  },
+  {
+    name: 'Projects',
+    href: '/projects'
+  },
+  {
+    name: 'Contacts',
+    href: '/contacts'
+  },
 ]
